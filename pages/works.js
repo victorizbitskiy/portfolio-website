460a02e8bebd076bac94ff2d16dc976c8143777c
victorizbitskiy <victor.izbitskiy@yandex.ru>
2023-04-14 21:38:14 +0300

    add ccgtb to works page

--- pages/works.js
+++ pages/works.js
@@ -1,12 +1,31 @@
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+
+import thumbCCGTB from '../public/images/works/ccgtb/ccgtb_01.png'
 
 const Works = () => {
   return (
     <Container>
-      <Heading as="h3" font-size={20} mb={4}>
+      <Heading as="h3" fontSize={20} mb={4}>
         Works
       </Heading>
+
+      <SimpleGrid columns={[1, 1, 1]} gap={6} >
+        <Section>
+          <WorkGridItem
+            id="CleanCodeGuidesBot"
+            title="Clean Code Guides Telegram Bot"
+            thumbnail={thumbCCGTB}
+          >
+            This is a Telegram bot that has been created to help you make your
+            code cleaner. It provides an interface to work with adaptations of
+            Robert C. Martin&apos;s Clean Code for several programming
+            languages.
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
     </Container>
   )
 }
