410a1a22b83e5fdde11bbf85002beb65275d257f
victorizbitskiy <victor.izbitskiy@yandex.ru>
2023-04-15 08:35:37 +0300

    add page transitions with framer motion

--- pages/index.js
+++ pages/index.js
@@ -3,97 +3,100 @@ import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import NextLink from 'next/link'
 import { ChevronRightIcon } from "@chakra-ui/icons"
+import Layout from "../components/layouts/article"
 import { BioSection, BioYear } from "../components/bio"
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="7px"
-        bg={useColorModeValue('whiteAlpha.200', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hello, I&apos;m a full-stack developer based in Russia
-      </Box>
-
-      <Box display={{ md: "flex" }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title" >
-            Victor Izbitskiy
-          </Heading>
-          <p>Developer</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="7px"
+          bg={useColorModeValue('whiteAlpha.200', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/victorizbitskiy.jpg"
-            alt="Profile image"
-          />
+          Hello, I&apos;m a full-stack developer based in Russia
         </Box>
-      </Box>
 
-      <Section delay={0.1} >
-        <Heading as="h3" variant="section-title" >
-          Work
-        </Heading>
-        <Paragraph>
-          Hi! I&apos;m a full-stack developer with a passion for building digital stuff I want.
-          I love to find elegant solutions to problems through code and create useful things.
-          When not online, I like to go for walks, play chess and exercise.
-        </Paragraph>
-        <Box align="center" my={4}>
-          <Button
-            as={NextLink}
-            href="/works"
-            scroll={false}
-            rightIcon={<ChevronRightIcon />}
-            colorScheme="teal"
+        <Box display={{ md: "flex" }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title" >
+              Victor Izbitskiy
+            </Heading>
+            <p>Developer</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
           >
-            My portfolio
-          </Button>
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/victorizbitskiy.jpg"
+              alt="Profile image"
+            />
+          </Box>
         </Box>
-      </Section>
 
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1991</BioYear>
-          Born in Russia
-        </BioSection>
-        <BioSection>
-          <BioYear>2014</BioYear>
-          Completed the Master&apos;s Program in the Faculty of Computer Science and Applied Mathematics at the Belgorod State National Research University.
-        </BioSection>
-        <BioSection>
-          <BioYear>2017 to present</BioYear>
-          Worked as an ABAP developer
-        </BioSection>
-      </Section>
+        <Section delay={0.1} >
+          <Heading as="h3" variant="section-title" >
+            Work
+          </Heading>
+          <Paragraph>
+            Hi! I&apos;m a full-stack developer with a passion for building digital stuff I want.
+            I love to find elegant solutions to problems through code and create useful things.
+            When not online, I like to go for walks, play chess and exercise.
+          </Paragraph>
+          <Box align="center" my={4}>
+            <Button
+              as={NextLink}
+              href="/works"
+              scroll={false}
+              rightIcon={<ChevronRightIcon />}
+              colorScheme="teal"
+            >
+              My portfolio
+            </Button>
+          </Box>
+        </Section>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>
-          Chess, Art, Music, Drawing
-        </Paragraph>
-      </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1991</BioYear>
+            Born in Russia
+          </BioSection>
+          <BioSection>
+            <BioYear>2014</BioYear>
+            Completed the Master&apos;s Program in the Faculty of Computer Science and Applied Mathematics at the Belgorod State National Research University.
+          </BioSection>
+          <BioSection>
+            <BioYear>2017 to present</BioYear>
+            Worked as an ABAP developer
+          </BioSection>
+        </Section>
 
-    </Container>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ♥
+          </Heading>
+          <Paragraph>
+            Chess, Art, Music, Drawing
+          </Paragraph>
+        </Section>
+
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -4,29 +4,32 @@ import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
 import thumbCCGTB from '../public/images/works/ccgtb/ccgtb_01.png'
+import Layout from '../components/layouts/article'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
 
-      <SimpleGrid columns={[1, 1, 1]} gap={6} >
-        <Section>
-          <WorkGridItem
-            id="CleanCodeGuidesBot"
-            title="Clean Code Guides Telegram Bot"
-            thumbnail={thumbCCGTB}
-          >
-            This is a Telegram bot that has been created to help you make your
-            code cleaner. It provides an interface to work with adaptations of
-            Robert C. Martin&apos;s Clean Code for several programming
-            languages.
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+        <SimpleGrid columns={[1, 1, 1]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="CleanCodeGuidesBot"
+              title="Clean Code Guides Telegram Bot"
+              thumbnail={thumbCCGTB}
+            >
+              This is a Telegram bot that has been created to help you make your
+              code cleaner. It provides an interface to work with adaptations of
+              Robert C. Martin&apos;s Clean Code for several programming
+              languages.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
