79e9ef3a8a8f85664c8c7552d52fe7be814dd6be
victorizbitskiy <victor.izbitskiy@yandex.ru>
2023-04-19 19:44:31 +0300

    add bender model and refactoring

--- .nextconfig.js
+++ .nextconfig.js
@@ -1,4 +1,3 @@
 module.exports = {
-   reactStrictmode: true,
-   trailingSlash: true
+   reactStrictmode: true
 }
--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,11 +1,11 @@
 import { Box, Container } from "@chakra-ui/react";
 import Navbar from '../navbar'
 import Head from "next/head";
-import Vocel from "../voxel";
+import THREEObject from "../THREEObject";
 
 const Main = ({ children, router }) => {
   return (
-    <Box as="main" pb={8}>
+    <Box as="main" pb={0}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Victor Izbitskiy - Homepage</title>
@@ -14,7 +14,7 @@ const Main = ({ children, router }) => {
       <Navbar path={router.asPath} />
 
       <Container maxW={"container.md"} pt={14}>
-        <Vocel />
+        <THREEObject />
         {children}
       </Container>
     </Box>
